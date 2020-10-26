19ed6a8bdf87a9419cf300b3cbc5b9aed88ab419
Nicholas Silas <njsilas@gmail.com>
2020-10-25 20:45:39 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size >= 3 
+    ending = array.pop
+      array.push('and '+ending)
+      array.join(', ')
+    elsif array.size >= 1
+      array.join(' and ')  
+    else
+      array.join  
+    end  
 end
